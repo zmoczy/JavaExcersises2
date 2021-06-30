1152a6dcd886b834a3a84bc2c592b536810d9611
Zachary Moczygemba <zachary_moczygemba@hcl.com>
2021-06-30 14:01:49 -0500

    testing .gitIgnore

---
+++ JavaExcersies2/src/main/java/com/javaExcersies/JavaExcersies2/App.java
@@ -0,0 +1,13 @@
+package com.javaExcersies.JavaExcersies2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
